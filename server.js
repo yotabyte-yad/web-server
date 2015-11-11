ed003c72d60baa48d3552e162528b92fa863af86
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2015-11-11 20:11:15 +0530

    Added exclaimationgit add .startup.bat

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(middleware.logger);
 // });
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('About Us');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
